7559216792e88b01a34d9418dfadb8efba462fb3
f4ayush <rajanand.ayush98@gmail.com>
2023-08-06 11:34:30 +0530

    added validation messages

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -1,65 +1,99 @@
-import React, {useEffect, useState} from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { createGroup, getGroups, deleteGroup } from "../redux/slices/groups";
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Button from '@mui/material/Button';
-import Input from '@mui/material/Input';
-import Typography from '@mui/material/Typography';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import AddIcon from '@mui/icons-material/Add';
-
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  createGroup,
+  getGroups,
+  deleteGroup,
+  editGroup,
+} from "../redux/slices/groups";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Alert from "@mui/material/Alert";
+import Typography from "@mui/material/Typography";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import AddIcon from "@mui/icons-material/Add";
 
 function TodoGroup() {
   const groups = useSelector(getGroups);
   const dispatch = useDispatch();
-  const handleAddGroup = (e)=>{
-    dispatch(createGroup({id: groups.groups.length+1}))
-    
-  }
-  const updateFrom = (event) =>{
-    // const newValue = groups.filter(group=> group.id === event.target.data)
-    
-  }
+  const [groupDetail, setGroupDetail] = useState([]);
 
-  const updateTo = (event) =>{
-    // const newValue = groups.filter(group=> group.id === event.target.data)
+  useEffect(() => {
+    setGroupDetail(groups.groups);
+  }, [groups]);
 
-  }
+  const handleAddGroup = (e) => {
+    dispatch(createGroup({ id: groups.groups.length + 1, from: 0, to: 0 }));
+  };
+  const updateFrom = (event, gId) => {
+    console.log(event.target.value);
+    const newGroups = groupDetail.map((group) => {
+      if (group.id == gId) {
+        console.log("yes");
+        return { ...group, from: event.target.value };
+      }
+      return group;
+    });
+
+    dispatch(
+      editGroup({ value: parseInt(event.target.value) || 0, gId, from: true })
+    );
+
+    setGroupDetail(newGroups);
+  };
+
+  const updateTo = (event, gId) => {
+    console.log(event.target.value);
+    const newGroups = groupDetail.map((group) => {
+      if (group.id == gId) {
+        console.log("yes");
+        return { ...group, to: event.target.value };
+      }
+      return group;
+    });
+    setGroupDetail(newGroups);
+    dispatch(
+      editGroup({ value: parseInt(event.target.value) || 0, gId, to: true })
+    );
+  };
   return (
     <Box maxWidth="sm">
-      {
-        groups.groups.map(group=>(
-          <Stack key={group.id} direction="row" useFlexGap flexWrap="wrap">
-            <Stack direction="row" gap="10%">
-              {console.log(group)}
-              <DeleteIcon  onClick={()=> dispatch(deleteGroup(group.id))}/>
-              <Stack direction="row">
-                <Button variant="outlined">{`Group ${group.id}`}</Button>
-                <Input variant="outlined" data-id={group.id} value={group.from} onClick={updateFrom}/>
-                <Button variant="outlined"><ArrowForwardIcon/></Button>
-                <Input variant="outlined" data-id={group.id} value={group.to} onChange={updateFrom}/>
-              </Stack>
-              
-              <Typography sx={{cursor:"pointer"}}>
-                {
-                  group.todos?.reduce((ac,todo)=>(
-                    ` ${ac},(${todo.id}) ${todo.completed}`
-                  ),"").replace(",","")
-                }
-
-                {group.status}
-              </Typography>
+      {groups.groups.map((group) => (
+        <Stack key={group.id} direction="row" useFlexGap flexWrap="wrap">
+          <Stack direction="row" gap="10%">
+            {console.log(group)}
+            <DeleteIcon onClick={() => dispatch(deleteGroup(group.id))} />
+            <Stack direction="row">
+              <Button variant="outlined">{`Group ${group.id}`}</Button>
+              <TextField
+                hiddenLabel
+                variant="outlined"
+                value={group.from}
+                onChange={(e) => updateFrom(e, group.id)}
+              />
+              <Button variant="outlined">
+                <ArrowForwardIcon />
+              </Button>
+              <TextField
+                variant="outlined"
+                value={group.to}
+                onChange={(e) => updateTo(e, group.id)}
+              />
             </Stack>
+            <Typography sx={{ cursor: "pointer" }}>{group.status}</Typography>
+          </Stack>
         </Stack>
-        ))
-      }
+      ))}
 
-     
-      <Typography sx={{cursor:"pointer"}} onClick={handleAddGroup}><AddIcon/> Add Items</Typography>
+      {groups.error && <Alert severity="error">{groups.error}</Alert>}
+      <Typography sx={{ cursor: "pointer" }} onClick={handleAddGroup}>
+        <AddIcon /> Add Items
+      </Typography>
     </Box>
-  )
+  );
 }
 
-export default TodoGroup
+export default TodoGroup;
--- src/redux/slices/groups.js
+++ src/redux/slices/groups.js
@@ -5,6 +5,7 @@ const initialState = {
   groups: [],
   isValid: true,
   status: "idle",
+  error: ""
 };
 export const todoSlice = createSlice({
   initialState,
@@ -16,8 +17,18 @@ export const todoSlice = createSlice({
       state.groups = newState;
     },
     editGroup: (state, action) => {
-      const updatedGroup = state.groups.filter((group) => group.id != action.payload.id)
-      state = [...updatedGroup, action.payload]
+      state.groups = state.groups.map(group=>{
+        if(group.id == action.payload.gId){
+          if(action.payload.from){
+            group.from = action.payload.value
+          }else if(action.payload.to){
+            group.to = action.payload.value
+          }
+        }
+        return group
+      })
+
+      console.log(state.groups)
     },
     deleteGroup: (state, action) => {
       console.log(action.payload);
@@ -34,52 +45,56 @@ export const todoSlice = createSlice({
       })
       .addCase(getStatusAsync.fulfilled, (state, action) => {
         state.status = "idle";
-        state.groups = action.payload;
+        state.groups = action.payload.groups;
+        state.error = action.payload.error;
         console.log(state, "asas")
       });
   },
 });
 
-export const { deleteGroup, createGroup } = todoSlice.actions;
+export const { deleteGroup, createGroup, editGroup } = todoSlice.actions;
 export default todoSlice.reducer;
 
 export const getGroups = (state) => state.groups;
 
 function isPartitionValid(groups) {
   // Rule 1: Check if the entire range of 1-10 is covered
+  console.log(groups)
+
   const groupArr = groups.map((group) => [group.from, group.to]);
   const sortedGroups = groupArr.flat().sort((a, b) => a - b);
+  const sortedArr = groupArr.sort((a, b) => a[0] - b[0]);
+
   console.log(sortedGroups);
   if (sortedGroups[0] !== 1 || sortedGroups[sortedGroups.length - 1] !== 10) {
-    return false;
+    return {isValid:false, errorMessage: "All todos not present"};
   }
-  console.log("groupSubset", 2);
+  console.log("groupSubset", 2, groupArr);
   // Rule 2: Check if there are no gaps between consecutive groups
-  for (let i = 0; i < groupArr.length - 1; i++) {
-    if (groupArr[i + 1] - groupArr[i] !== 1) {
-      return false;
-    }
-  }
-  console.log("groupSubset", 3);
-  // Rule 3: Check if there is no overlap between consecutive groups
-  for (let i = 0; i < groupArr.length - 1; i++) {
-    const currentGroup = groupArr[i];
-    const nextGroup = groupArr[i + 1];
-    if (currentGroup[1] >= nextGroup[0]) {
-      return false;
-    }
-  }
+  for (let i = 0; i < sortedArr.length - 1; i++) {
+    const currentSubset = sortedArr[i];
+    const nextSubset = sortedArr[i + 1];
 
-  console.log(groupArr);
+    // Check for gaps
+    if (currentSubset[1] + 1 !== nextSubset[0]) {
+      return {isValid:false, errorMessage: "There is a gap between todos"};
+    }
+
+    // Check for overlap
+    if (currentSubset[1] >= nextSubset[0]) {
+      return {isValid:false, errorMessage: "There is a overlap in todos"};
+    }
+  }
   // All rules are satisfied
-  return true;
+  return {isValid:true, errorMessage: ""};
 }
 
 export const getStatusAsync = createAsyncThunk(
   "groups/fetchStatus",
   async (groups) => {
     console.log(groups);
-    if (isPartitionValid(groups)) {
+    const partitionCheck = isPartitionValid(groups)
+    if (partitionCheck.isValid) {
       const updatedGroup = await groups.map(async (group) => {
         let status = "";
         for (let i = group.from; i <= group.to; i++) {
@@ -89,12 +104,12 @@ export const getStatusAsync = createAsyncThunk(
           });
           status += `(${i}) ${response.data.completed},`;
         }
-    
-        // group.taskStatus = status
         return { ...group, status };
       });
       console.log(await Promise.all(updatedGroup))
-      return await Promise.all(updatedGroup)
+      return {groups: await Promise.all(updatedGroup), error: ''}
+    }else{
+      return {groups, error: partitionCheck.errorMessage}
     }
   }
 );
